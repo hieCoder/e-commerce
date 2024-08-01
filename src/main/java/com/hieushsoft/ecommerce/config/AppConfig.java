abf60a28809486220277eb1f2440dfddf1dc70a9
hieDev <hieuzzz601@gmail.com>
2024-08-01 16:55:13 +0700

    .

--- src/main/java/com/hieushsoft/ecommerce/config/AppConfig.java
+++ src/main/java/com/hieushsoft/ecommerce/config/AppConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,6 +14,9 @@ public class AppConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
+        httpSecurity.sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(Authorize -> Authorize
+                .requestMatchers("/api/admin/**").hasAnyRole("RESTAURANT_OWNER", "ADMIN"));
 
         return  null;
     }
